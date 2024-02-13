b6f3d026f528a3681f87c39559810277907d44f2
jbosss1 <159961786+jbosss1@users.noreply.github.com>
2024-02-14 01:36:43 +0200

    Update ApplicationStatusHandler.java

--- ApplicationStatusHandler.java
+++ ApplicationStatusHandler.java
@@ -10,6 +10,10 @@ public class ApplicationStatusHandler implements Handler {
 
     @Override
     public ApplicationStatusResponse performOperation(String id) {
+        if (id == null) {
+            return new ApplicationStatusResponse.Failure(null, 0);
+        }
+
         long startTime = System.currentTimeMillis();
         int retriesCount = 0;
 
@@ -28,20 +32,28 @@ public class ApplicationStatusHandler implements Handler {
         return new ApplicationStatusResponse.Failure(null, retriesCount);
     }
 
-    private ApplicationStatusResponse processResponses(Response response1, Response response2) {
-        if (response1 instanceof Response.Success success1) {
-            return new ApplicationStatusResponse.Success(success1.applicationId(), success1.applicationStatus());
-        } else if (response2 instanceof Response.Success success2) {
-            return new ApplicationStatusResponse.Success(success2.applicationId(), success2.applicationStatus());
-        } else if (response1 instanceof Response.RetryAfter retryAfter1 && response2 instanceof Response.RetryAfter retryAfter2) {
-            Duration maxDelay = retryAfter1.delay().compareTo(retryAfter2.delay()) > 0 ? retryAfter1.delay() : retryAfter2.delay();
-            sleep(maxDelay);
-        } else if (response1 instanceof Response.Failure failure1 && response2 instanceof Response.Failure failure2) {
-            return new ApplicationStatusResponse.Failure(Duration.ofMillis(System.currentTimeMillis()), 0);
-        }
+private ApplicationStatusResponse processResponses(Response response1, Response response2) {
+    if (response1 instanceof Response.Success success1) {
+        return new ApplicationStatusResponse.Success(success1.applicationId(), success1.applicationStatus());
+    }
 
+    if (response2 instanceof Response.Success success2) {
+        return new ApplicationStatusResponse.Success(success2.applicationId(), success2.applicationStatus());
+    }
+
+    if (response1 instanceof Response.RetryAfter retryAfter1 && response2 instanceof Response.RetryAfter retryAfter2) {
+        Duration maxDelay = retryAfter1.delay().compareTo(retryAfter2.delay()) > 0 ? retryAfter1.delay() : retryAfter2.delay();
+        sleep(maxDelay);
         return null;
     }
+
+    if (response1 instanceof Response.Failure && response2 instanceof Response.Failure) {
+        return new ApplicationStatusResponse.Failure(Duration.ofMillis(System.currentTimeMillis()), 0);
+    }
+
+    return null;
+}
+
 
     private void sleep(Duration duration) {
         try {
